85ba4e1701db1d71c0110067a104e9d3e799cbb9
ahmed1349 <brillianta134@gmail.com>
2025-10-31 16:47:36 +0200

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,260 @@
+const navLinks = [
+  {
+    name: "Work",
+    link: "#work",
+  },
+  {
+    name: "Experience",
+    link: "#experience",
+  },
+  {
+    name: "Skills",
+    link: "#skills",
+  },
+  {
+    name: "Testimonials",
+    link: "#testimonials",
+  },
+];
+
+const words = [
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+];
+
+const counterItems = [
+  { value: 3, suffix: "+", label: "Years of Experience" },
+  { value: 20, suffix: "+", label: "Satisfied Clients" },
+  { value: 10, suffix: "+", label: "Completed Projects" },
+  { value: 90, suffix: "%", label: "Client Retention Rate" },
+];
+
+const logoIconsList = [
+  {
+    imgPath: "/images/logos/company-logo-1.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-2.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-3.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-4.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-5.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-6.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-7.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-8.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-9.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-10.png",
+  },
+
+];
+
+const abilities = [
+  {
+    imgPath: "/images/seo.png",
+    title: "Quality Focus",
+    desc: "Delivering high-quality results while maintaining attention to every detail.",
+  },
+  {
+    imgPath: "/images/chat.png",
+    title: "Reliable Communication",
+    desc: "Keeping you updated at every step to ensure transparency and clarity.",
+  },
+  {
+    imgPath: "/images/time.png",
+    title: "On-Time Delivery",
+    desc: "Making sure projects are completed on schedule, with quality & attention to detail.",
+  },
+];
+
+const techStackImgs = [
+  {
+    name: "React Developer",
+    imgPath: "/images/logos/react.png",
+  },
+  {
+    name: "Python Developer",
+    imgPath: "/images/logos/python.svg",
+  },
+  {
+    name: "Backend Developer",
+    imgPath: "/images/logos/node.png",
+  },
+  {
+    name: "Interactive Developer",
+    imgPath: "/images/logos/three.png",
+  },
+  {
+    name: "Project Manager",
+    imgPath: "/images/logos/git.svg",
+  },
+];
+
+const techStackIcons = [
+  {
+    name: "React Developer",
+    modelPath: "/models/react_logo-transformed.glb",
+    scale: 1,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Python Developer",
+    modelPath: "/models/python-transformed.glb",
+    scale: 0.8,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Backend Developer",
+    modelPath: "/models/node-transformed.glb",
+    scale: 5,
+    rotation: [0, -Math.PI / 2, 0],
+  },
+  {
+    name: "Interactive Developer",
+    modelPath: "/models/three.js-transformed.glb",
+    scale: 0.05,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Project Manager",
+    modelPath: "/models/git-svg-transformed.glb",
+    scale: 0.05,
+    rotation: [0, -Math.PI / 4, 0],
+  },
+];
+
+const expCards = [
+  {
+    review: "Ahmed combined creativity with strong technical expertise to greatly enhance our frontend performance, playing a key role in delivering faster and more efficient user experiences.",
+    imgPath: "/images/logo01.png",
+    logoPath: "/images/logo01.png",
+    title: "Software Engineer && Frontend Developer",
+    date: "OCT 2024 - Part-time - Present",
+    responsibilities: [
+      "Communicated directly with clients to gather requirements and translate their needs into functional, visually appealing web solutions.",
+      "Designed and developed responsive frontend interfaces, ensuring smooth interaction and optimal user experience across all devices.",
+      "Collaborated with UI/UX designers to implement intuitive layouts and maintain design consistency.",
+      "Optimized web applications for maximum speed and scalability.",
+    ],
+  },
+  {
+    review: "“Ahmed played a crucial role in the success of the Ithraa App. His ability to blend technical skill with creative design made the app stand out. He took full ownership of the UI/UX design and the app publishing process, ensuring everything was delivered on time and with high quality. His leadership, attention to detail, and problem-solving mindset were invaluable to our team.”.",
+    imgPath: "/images/logo02.png",
+    logoPath: "/images/logo02.png",
+    title: "Software Engineer",
+    date: "Apr 2025 - OCT 2025",
+    responsibilities: [
+      "Planned and structured the app workflow, defining a smooth user journey from onboarding to core features.",
+      "Collaborated closely with the development team to ensure accurate implementation of design elements and functionality.",
+      "Handled the app deployment process, publishing on both Google Play Store and Apple App Store.",
+      "Lead the project timeline, managing deliverables and ensuring successful completion within 5 months",
+    ],
+  },
+  {
+    review: "Healgen is an intelligent healthcare platform designed for both mobile and web users, with a focus on culturally-aware digital care.",
+    imgPath: "/images/logo04.png",
+    logoPath: "/images/logo03.png",
+    title: "AI developer && React Developer",
+    date: "Aug 2024 - June 2025",
+    responsibilities: [
+      "Built a healthcare platform using React, integrating with Django backend.",
+      "Developed AI-driven features to provide personalized healthcare recommendations.",
+      "Fine-tuned LLM to improve accuracy and reliability of health predictions.",
+      "Improved app performance and user experience through code optimization and testing.",
+      "Lead the team of developers to ensure timely delivery of project milestones.",
+    ],
+  },
+];
+
+const expLogos = [
+  {
+    name: "logo1",
+    imgPath: "/images/logo1.png",
+  },
+  {
+    name: "logo2",
+    imgPath: "/images/logo2.png",
+  },
+  {
+    name: "logo3",
+    imgPath: "/images/logo3.png",
+  },
+];
+
+const testimonials = [
+  {
+    name: "Ithraaldmam",
+    mentions: "@eithraaldmam",
+    review:
+      "Ahmed played a crucial role in the success of the Ithraa App. His ability to blend technical skill with creative design made the app stand out.",
+    imgPath: "/images/logo02.png",
+  },
+  {
+    name: "CEO- Hanadi",
+    mentions: "@hanadi",
+    review:
+      "Ahmed combined creativity with strong technical expertise to greatly enhance our frontend performance, playing a key role in delivering faster and more efficient user experiences.",
+    imgPath: "/images/logo01.png",
+  },
+
+  {
+    name: "Marvin McKinney",
+    mentions: "@marvinmckinney",
+    review:
+      "Adrian was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
+    imgPath: "/images/client5.png",
+  },
+];
+
+const socialImgs = [
+  {
+    name: "insta",
+    imgPath: "/images/insta.png",
+  },
+  {
+    name: "fb",
+    imgPath: "/images/fb.png",
+  },
+  {
+    name: "x",
+    imgPath: "/images/x.png",
+  },
+  {
+    name: "linkedin",
+    imgPath: "/images/linkedin.png",
+  },
+];
+
+export {
+  words,
+  abilities,
+  logoIconsList,
+  counterItems,
+  expCards,
+  expLogos,
+  testimonials,
+  socialImgs,
+  techStackIcons,
+  techStackImgs,
+  navLinks,
+};

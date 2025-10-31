6839b0e9dd1c685afe382351000cef622ae4a6bc
ahmed1349 <brillianta134@gmail.com>
2025-10-31 17:30:18 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss()],
-  base: '/ahmed1349.github.io/',
+  plugins: [react(), tailwindcss()],
+  base: '/', // ✅ for user GitHub Pages
 })

b23bdd15de25417d6804dc969fce1875ffaa6928
ahmed1349 <brillianta134@gmail.com>
2025-10-31 16:58:14 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://ahmed1349.github.io/',
   plugins: [react(),
   tailwindcss()],
 })
